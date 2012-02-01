df900bbacae24069b3bb18e4664b03c055dd5a76
Christian G. Warden <cwarden@xerus.org>
2012-01-31 18:34:33 -0800

    make exim into parameterized class

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,46 +2,51 @@
 # Copyright (C) 2007 admin@immerda.ch
 #
 
-class exim {
-	require exim::params
+class exim(
+  $manage_shorewall = $exim::params::manage_shorewall,
+  $localonly = $exim::params::localonly,
+  $munin_checks = $exim::params::munin_checks,
+  $nagios_checks = $exim::params::nagios_checks,
+  $ports = $exim::params::ports
+  ) inherits exim::params {
 	
 	include exim::install, exim::config, exim::service
 
-	if $exim::params::munin_checks {
+	if $munin_checks {
 		include exim::munin
 	}
 
-	if $exim::params::manage_shorewall {
+	if $manage_shorewall {
 		include shorewall::rules::out::smtp
 	}
 
-	if !$exim::params::localonly {
-		if $exim::params::manage_shorewall {
-			if array_include($exim::params::ports,'25') {
+	if !$localonly {
+		if $manage_shorewall {
+			if array_include($ports,'25') {
 				include shorewall::rules::smtp
 			}
 
-			if array_include($exim::params::ports,'587') {
+			if array_include($ports,'587') {
 				include shorewall::rules::smtp_submission
 			}
 
-			if array_include($exim::params::ports,'465') {
+			if array_include($ports,'465') {
 				include shorewall::rules::smtps
 			}
 		}
 
-		if $exim::params::nagios_checks {
-			exim::nagios { $exim::params::ports:
-				checks    => $exim::params::nagios_checks,
-				cert_days => $exim::params::nagios_checks['cert_days'],
-				host      => $exim::params::nagios_checks['hostname']
+		if $nagios_checks {
+			exim::nagios { $ports:
+				checks    => $nagios_checks,
+				cert_days => $nagios_checks['cert_days'],
+				host      => $nagios_checks['hostname']
 			}
 
 			nagios::service { "dnsbl_${fqdn}":
-				check_command => "check_dnsbl!${exim::params::nagios_checks['hostname']}"
+				check_command => "check_dnsbl!${nagios_checks['hostname']}"
 			}
 
-			if $exim::params::nagios_checks['dnsbl'] != true {
+			if $nagios_checks['dnsbl'] != true {
 				Nagios::Service["dnsbl_${fqdn}"] {
 					ensure => absent
 				}
